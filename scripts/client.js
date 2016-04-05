2bcfa129dc252b86d19d25a4d1f3a65b10b119ee
Jerome Lecomte <jerle76@gmail.com>
2016-04-05 14:42:00 -0400

    basic Firebase tutorial

--- scripts/client.js
+++ scripts/client.js
@@ -1,1 +1,6 @@
-var myDataRef = new Firebase('https://office-to-logo.firebaseio.com/');
+(function() {
+  'use strict';
+
+  var myDataRef = new Firebase('https://office-to-logo.firebaseio.com/');
+  myDataRef.push({ user: navigator.userAgent, msg: 'Hello World!' });
+})();
--- scripts/render.js
+++ scripts/render.js
@@ -1,1 +1,33 @@
-var myDataRef = new Firebase('https://office-to-logo.firebaseio.com/');
+(function() {
+  'use strict';
+
+  createjs.Ticker.setFPS(30);
+
+  // resize canvas to fit screen
+  var canvas = document.getElementById('canvas');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  // setup CreateJS objects
+  var stage = new createjs.Stage('canvas');
+  var renderContainer = new createjs.Container();
+  var logoContainer = new createjs.Container();
+  var inputContainer = new createjs.Container();
+  stage.addChild(renderContainer, logoContainer, inputContainer);
+
+  // main rendering loop
+  var update = function(event) {
+    stage.update();
+  };
+
+  createjs.Ticker.addEventListener('tick', update);
+
+
+
+  var myDataRef = new Firebase('https://office-to-logo.firebaseio.com/');
+  myDataRef.on('child_added', function(snapshot) {
+    var stroke = snapshot.val();
+    console.log(stroke.user, stroke.msg);
+  });
+
+})();
