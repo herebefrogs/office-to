6d49c3bc5e90b0494188f69b1079659cd53d12a6
Jerome Lecomte <jerle76@gmail.com>
2016-04-05 22:40:49 -0400

    even more terrible math to try to get the input circle to move in a way that makes sense relative to how the phone is shaken

--- scripts/client.js
+++ scripts/client.js
@@ -10,13 +10,16 @@
     pos: {
       x: 320,
       y: 240
+    },
+    vel: {
+      x: 0,
+      y: 0
     }
   };
-  var elapsed_time = 0;
-  var FRAME_RATE = 250;
-  var previous_accel_x = 0;
-  var previous_accel_y = 0;
-  var previous_time = 0;
+  var elapsed_time = 0; // in seconds
+  var FRAME_RATE = 0.15; // in seconds
+  var VELOCITY_DRAG = 5; // velocity reduction per frame rate
+  var previous_time = 0; // in milliseconds
   var uid;
 
   // sign in anonymously
@@ -45,24 +48,35 @@
     document.innerHtml = 'you cannot participate';
   }
   window.addEventListener('devicemotion', function(event) {
-    elapsed_time += event.timeStamp - previous_time;
+    var delta_time = (event.timeStamp - previous_time) / 1000 / FRAME_RATE;
+    elapsed_time += delta_time;
     previous_time = event.timeStamp;
-    if (elapsed_time > FRAME_RATE) {
-      elapsed_time = 0;
 
-      // this is terrible and doesn't do anything that make sense
-      // TODO actually generate acceleration vectors...
-      data.pos.x += event.acceleration.x - previous_accel_x;
-      data.pos.y += event.acceleration.y - previous_accel_y;
+    // velocity from acceleration
+    // NOTE: when holding the phone straight, right is negative x and up is negative y
+    // so apply a negative sign to x to reverse that
+    // however CreateJS y axis points downward so keep the y acceleration value as is
+    data.vel.x += -event.acceleration.x * delta_time;
+    data.vel.y += event.acceleration.y * delta_time;
+/*
+    // position from velocity
+    data.pos.x += data.vel.x * delta_time + -event.acceleration.x * delta_time * delta_time;
+    data.pos.y += data.vel.y * delta_time + event.acceleration.y * delta_time * delta_time;
+*/
+    data.pos.x += data.vel.x;
+    data.pos.y += data.vel.y;
+
+    if (elapsed_time > FRAME_RATE) {
+      // apply drag (or circle never stops)
+      data.vel.x = Math.max(0, data.vel.x - VELOCITY_DRAG);
+      data.vel.y = Math.max(0, data.vel.y - VELOCITY_DRAG);
+
+      elapsed_time = 0;
       clientRef.set(data);
 
-      previous_accel_x = event.acceleration.x;
-      previous_accel_y = event.acceleration.y;
-
       if (DEBUG) {
-        document.getElementById('x').innerHTML = event.acceleration.x;
-        document.getElementById('y').innerHTML = event.acceleration.y;
-        document.getElementById('z').innerHTML = event.acceleration.z;
+        document.getElementById('x').innerHTML = data.vel.x;
+        document.getElementById('y').innerHTML = dat.vel.y;
       }
     }
   });
