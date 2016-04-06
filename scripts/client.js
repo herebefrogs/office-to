96cd97448ec7c4a8c8a401519a76f094d5ca4692
Jerome Lecomte <jerle76@gmail.com>
2016-04-06 11:00:31 -0400

    randomize client's paint brush color

--- scripts/client.js
+++ scripts/client.js
@@ -3,8 +3,80 @@
 
   var DEBUG = false;
   document.getElementById('debug').style.display = DEBUG ? 'block' : 'none';
+
+  // max excluded
+  var random = function(min, max) {
+    return Math.random() * (max - min) + min;
+  };
+  // max included
+  var randomInt = function(min, max) {
+    return Math.floor(random(min, max + 1));
+  };
+
   var clientRef;
+  var COLORS = [
+    // Material Design 100 level
+    '#FFCDD2',
+    '#F8BBD0',
+    '#E1BEE7',
+    '#D1C4E9',
+    '#C5CAE9',
+    '#BBDEFB',
+    '#B3E5FC',
+    '#B2EBF2',
+    '#B2DFDB',
+    '#C8E6C9',
+    '#DCEDC8',
+    '#F0F4C3',
+    '#FFF9C4',
+    '#FFECB3',
+    '#FFE0B2',
+    '#FFCCBC',
+    '#D7CCC8',
+    '#CFD8DC',
+    // Material Design 500 level
+    '#F44336',
+    '#E91E63',
+    '#9C27B0',
+    '#673AB7',
+    '#3F51B5',
+    '#2196F3',
+    '#03A9F4',
+    '#00BCD4',
+    '#009688',
+    '#4CAF50',
+    '#8BC34A',
+    '#CDDC39',
+    '#FFEB3B',
+    '#FFC107',
+    '#FF9800',
+    '#FF5722',
+    '#795548',
+    '#9E9E9E',
+    '#607D8B',
+    // Material Design 900 level
+    '#B71C1C',
+    '#880E4F',
+    '#4A148C',
+    '#311B92',
+    '#1A237E',
+    '#0D47A1',
+    '#01579B',
+    '#006064',
+    '#004D40',
+    '#1B5E20',
+    '#33691E',
+    '#827717',
+    '#F57F17',
+    '#FF6F00',
+    '#E65100',
+    '#BF360C',
+    '#3E2723',
+    '#212121',
+    '#263238'
+  ];
   var data = {
+    color: COLORS[randomInt(0, COLORS.length - 1)],
     pressed: false,
     pos: {
       x: 0,
@@ -26,15 +98,6 @@
   var strokesRef;
   var uid;
   var VELOCITY_DRAG = 1; // velocity reduction per frame rate
-
-  // max excluded
-  var random = function(min, max) {
-    return Math.random() * (max - min) + min;
-  };
-  // max included
-  var randomInt = function(min, max) {
-    return Math.floor(random(min, max + 1));
-  };
 
   // sign in anonymously
   var rootRef = new Firebase('https://office-to-logo.firebaseio.com/');
@@ -59,6 +122,9 @@
       clientRef.set(data);
 
       strokesRef = rootRef.child('strokes');
+
+      // show your current paint color
+      document.body.style.backgroundColor = data.color;
     }
   }, { remember: 'sessionOnly' });
 
@@ -112,7 +178,7 @@
       // add a new paint stroke
       if (data.vel.y > 1 || data.vel.x > 1) {
         strokesRef.push({
-          color: '#f00',
+          color: data.color,
           radius: randomInt(MIN_RADIUS, MAX_RADIUS),
           x: data.pos.x + randomInt(0, MAX_DRIFT),
           y: data.pos.y + randomInt(0, MAX_DRIFT),
