e98bd030f4feef22f3fc6b483ab7b92ed9f1eab0
Jerome Lecomte <jerle76@gmail.com>
2016-04-05 21:16:32 -0400

    debug code to analyse accelerometer data

--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,8 @@
 (function() {
   'use strict';
 
+  var DEBUG = false;
+  document.getElementById('debug').style.display = DEBUG ? 'block' : 'none';
   var clientRef;
   var data = {
     pressed: false,
@@ -11,6 +13,7 @@
     }
   };
   var elapsed_time = 0;
+  var FRAME_RATE = 250;
   var previous_accel_x = 0;
   var previous_accel_y = 0;
   var previous_time = 0;
@@ -44,7 +47,7 @@
   window.addEventListener('devicemotion', function(event) {
     elapsed_time += event.timeStamp - previous_time;
     previous_time = event.timeStamp;
-    if (elapsed_time > 1000) {
+    if (elapsed_time > FRAME_RATE) {
       elapsed_time = 0;
 
       // this is terrible and doesn't do anything that make sense
@@ -55,6 +58,12 @@
 
       previous_accel_x = event.acceleration.x;
       previous_accel_y = event.acceleration.y;
+
+      if (DEBUG) {
+        document.getElementById('x').innerHTML = event.acceleration.x;
+        document.getElementById('y').innerHTML = event.acceleration.y;
+        document.getElementById('z').innerHTML = event.acceleration.z;
+      }
     }
   });
 
