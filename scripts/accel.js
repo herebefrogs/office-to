76f5af533d64c38bf002afe08e41b0b568cd8c76
Jerome Lecomte <jerle76@gmail.com>
2016-04-29 13:24:35 -0400

    chart rotation rates

--- scripts/accel.js
+++ scripts/accel.js
@@ -4,6 +4,9 @@ var data_accel_y = [  ];
 var data_accel_grav_y = [  ];
 var data_accel_z = [  ];
 var data_accel_grav_z = [  ];
+var data_rot_x = [  ];
+var data_rot_y = [  ];
+var data_rot_z = [  ];
 
 
 var palette = new Rickshaw.Color.Palette();
@@ -36,7 +39,7 @@ var create_chart = function(id_label, data, legend_label, data2, legend_label2) 
   var graph = new Rickshaw.Graph( {
     element: chart_elt,
     width: 580,
-    height: 250,
+    height: 200,
     renderer: 'line',
     series: series
   } );
@@ -65,6 +68,9 @@ var graph_accel_x = create_chart('accel_x', data_accel_x, 'X acceleration', data
 var graph_accel_y = create_chart('accel_y', data_accel_y, 'Y acceleration', data_accel_grav_y, 'Y acceleration (including gravity)');
 var graph_accel_z = create_chart('accel_z', data_accel_z, 'Z acceleration', data_accel_grav_z, 'Z acceleration (including gravity)');
 
+var graph_rot_x = create_chart('rot_x', data_rot_x, 'X rotation rate');
+var graph_rot_y = create_chart('rot_y', data_rot_y, 'Y rotation rate');
+var graph_rot_z = create_chart('rot_z', data_rot_z, 'Z rotation rate');
 
 var start_time = Date.now();
 var THROTTLE = 10;
@@ -87,10 +93,16 @@ window.addEventListener('devicemotion', function(event) {
   process_data(event.accelerationIncludingGravity.y, data_accel_grav_y);
   process_data(event.acceleration.z, data_accel_z);
   process_data(event.accelerationIncludingGravity.z, data_accel_grav_z);
+  process_data(event.rotationRate.beta, data_rot_x);
+  process_data(event.rotationRate.gamma, data_rot_y);
+  process_data(event.rotationRate.alpha, data_rot_z);
 
   if (refresh) {
     graph_accel_x.render();
     graph_accel_y.render();
     graph_accel_z.render();
+    graph_rot_x.render();
+    graph_rot_y.render();
+    graph_rot_z.render();
   }
 });
